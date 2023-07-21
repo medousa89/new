00dd5465622218dd759c55ca54f9e0e37938e0c5
medousa89 <medousa89@gmail.com>
2023-07-22 00:21:16 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ rm -f userbouquet.*.tv
 wget "http://sgcpm.com/livestream/stream.xml"
 #python3 new.py
 python3 new1.py
-echo "#SERVICE 1:64:0:0:0:0:0:0:0:0::_____| ` TZ='Europe/Athens' date +"%d-%m-%Y %a %H.%M.%S"`|_____" >> "userbouquet.greekstreamtvATV.tv"
+echo "#SERVICE 1:64:1:0:0:0:0:0:0:0::_____|` TZ='Europe/Athens' date +"%d-%m-%Y_%a_%H_%M_%S"`|_____" >> "userbouquet.greekstreamtvATV.tv"
 rm -f stream.xml
