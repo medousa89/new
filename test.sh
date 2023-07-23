3e8594dcd04a56c4cc2f92e937e5b79f483c0c12
medousa89 <medousa89@gmail.com>
2023-07-23 09:04:14 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 rm -f userbouquet.*.tv
 python3 new1.py
-echo "#SERVICE 1:64:1:0:0:0:0:0:0:0::` TZ='Europe/Athens' date +"%d-%m-%Y_%a_%H_%M_%S"`" >> "userbouquet.greekstreamtvATV.tv"
+echo "#SERVICE 1:64:1:0:0:0:0:0:0:0::` TZ='Europe/Athens' date +"%d-%m-%Y%a%H.%M.%S"`" >> "userbouquet.greekstreamtvATV.tv"
 rm -f stream.xml
